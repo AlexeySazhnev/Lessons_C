4cd474bc6c27a0b10d9974dec17f10ec64360d32
Alexey <144210467+AlexeySazhnev@users.noreply.github.com>
2023-10-01 00:47:54 +0300

    Task27

--- Task27_DZ/Program.cs
+++ Task27_DZ/Program.cs
@@ -4,7 +4,7 @@
 // 9012 -> 12
 
 
-Console.Write("Введите целое число:");
+Console.WriteLine("Введите целое число:");
 int num = Convert.ToInt32(Console.ReadLine());
 
 int SumDigit(int num)
@@ -12,13 +12,15 @@ int SumDigit(int num)
     if (num < 0) num *= -1;
     int sum = 0;
 
-    for (int i = 1; num % 10 < 1; i++)
+    while (num != 0)
     {
         int digit = num % 10;
-        sum = sum + digit;
+        sum += digit;
+        num /= 10;
     }
+
     return sum;
 }
 
 int sumDigit = SumDigit(num);
-Console.Write($"{num} -> {sumDigit}");
+Console.WriteLine($"{num} -> {sumDigit}");
