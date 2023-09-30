f385d916f3f1ee0adf07382671915e67c3a2848e
Alexey <144210467+AlexeySazhnev@users.noreply.github.com>
2023-09-30 23:25:17 +0300

    Решение Task25

---
+++ Task25_DZ/Program.cs
@@ -0,0 +1,22 @@
+﻿// 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+Console.WriteLine("Введите число A:");
+int numA = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите число B:");
+int numB = Convert.ToInt32(Console.ReadLine());
+
+int Degree(int numA, int numB)
+{
+    int mult = 1;
+
+    for (int i = 1; i <= numB; i++)
+    {
+        mult = mult * numA;
+    }
+    return mult;
+}
+
+int result = Degree(numA, numB);
+Console.WriteLine($"{numA}, {numB} -> {result}");
