0f30c0ababad4cccba1df95ea464bc5859d9ce2e
Alexey <144210467+AlexeySazhnev@users.noreply.github.com>
2023-10-01 10:30:26 +0300

    решение Task29

---
+++ Task29_DZ/Program.cs
@@ -0,0 +1,45 @@
+﻿// Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int[] RandomArray(int arrLength)
+{
+    int[] array = new int[arrLength];
+    Random rand = new Random();
+    for (int i = 0; i < arrLength; i++)
+    {
+        array[i] = rand.Next(100);
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.Write($"{array[i]} ->");
+        }
+    }
+    Console.Write("[");
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (i < array.Length - 1)
+        {
+            Console.Write($"{array[i]}, ");
+        }
+        else
+        {
+            Console.WriteLine($"{array[i]}]");
+        }
+    }
+}
+
+int[] arr = RandomArray(8);
+PrintArray(arr);
