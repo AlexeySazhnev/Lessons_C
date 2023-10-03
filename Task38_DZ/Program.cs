2271e93795b8f9387a881cafc6f697e4e68b811d
Alexey <144210467+AlexeySazhnev@users.noreply.github.com>
2023-10-03 17:15:22 +0300

    Решение Task38

---
+++ Task38_DZ/Program.cs
@@ -0,0 +1,66 @@
+﻿// 38: Задайте массив вещественных чисел.
+// Найдите разницу между максимальным и минимальным
+// элементов массива.
+
+// [3 7 22 2 78] -> 76
+
+double[] CreateArrarRndDouble(int size, int min, int max)
+{
+    double[] arr = new double[size];
+    Random rnd = new Random();
+
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.NextDouble() * (max - min) + min;
+    }
+
+    return arr;
+}
+
+double MaxElement(double[] arr)
+{
+    double max = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] > max) max = arr[i];
+    }
+
+    return max;
+}
+
+double MinElement(double[] arr)
+{
+    double min = arr[0];
+    for (int i = 1; i < arr.Length; i++)
+    {
+        if (arr[i] < min) min = arr[i];
+    }
+
+    return min;
+}
+
+void DiffMaxandMin(double[] arr)
+{
+    double max = MaxElement(arr);
+    double min = MinElement(arr);
+    double result = max - min;
+
+    Console.Write($" -> {result:F1}");
+}
+
+void PrintArrayDouble(double[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (i < arr.Length - 1) Console.Write($"{arr[i]:F1}: ");
+        else Console.Write($"{arr[i]:F1}");
+    }
+    Console.Write("]");
+}
+
+double[] array = CreateArrarRndDouble(5, -99, 100);
+PrintArrayDouble(array);
+DiffMaxandMin(array);
+
+
