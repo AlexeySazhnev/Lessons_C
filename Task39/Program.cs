6cc317fce9c07e10319fd01d384d40793b605511
Alexey <144210467+AlexeySazhnev@users.noreply.github.com>
2023-10-05 12:09:41 +0300

    Решение Task439

--- Task39/Program.cs
+++ Task39/Program.cs
@@ -3,10 +3,31 @@
 // месте, а первый - на последнем и т.д.)
 // [1 2 3 4 5] -> [5 4 3 2 1]
 // [6 7 3 6] -> [6 3 7 6]
+int[] CreateArrarRndint(int size, int min, int max)
+{
+    int[] arr = new int[size];
+    Random rnd = new Random();
 
+    for (int i = 0; i < size; i++)
+    {
+        arr[i] = rnd.Next(min, max + 1);
+    }
+
+    return arr;
+}
+
+void PrintOut(int[] arr)
+{
+    Console.Write("[");
+    for (int i = 0; i < arr.Length - 1; i++)
+    {
+        Console.Write($"{arr[i]}, ");
+    }
+    Console.WriteLine($"{arr[^1]}]");// ^1 последний элемент массива
+}
 void ReverseArray(int[] arr)
 {
-    for (int i = 1; i < arr.Length / 2; i++)
+    for (int i = 0; i < arr.Length / 2; i++)
     {
         int temp = arr[i];// 2
         arr[i] = arr[arr.Length - 1 - i];
@@ -14,10 +35,10 @@ void ReverseArray(int[] arr)
     }
 }
 
-int[] array = CreateArrarRndInt(5, 1, 9);
-PrintArray(array);
+int[] array = CreateArrarRndint(5, 1, 9);
+PrintOut(array);
 ReverseArray(array);
-PrintArray(array);
+PrintOut(array);
 
 // Array.Reverse(array);
 // PrintArray(array)
